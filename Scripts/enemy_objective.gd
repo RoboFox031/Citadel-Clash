d2fd65329b9717781d4a44d8a834d65a00857048
Zane <zagrismore@gmail.com>
2024-09-22 10:07:13 -0400

    Finished Enemy Objective

--- Scripts/enemy_objective.gd
+++ Scripts/enemy_objective.gd
@@ -1,14 +1,16 @@
 extends Area2D
 
+@onready var main_path: Path2D = $"../mainPath"
+
 func _on_body_entered(body: Node2D) -> void:
 	if(body.name == "Enemy"):
 		#life losing goes here
-		print(body.get_parent().get_child_count())
-		if(body.get_parent().get_child_count()==0):
-			print("All Enemies Defeated")
 		body.get_parent().queue_free()
 		#deletes the enemy from the scene
 		print("Enemy has made it!")
-		print(body.get_parent().get_children())
+		#For whatever reason, it deletes the enemy after the if,
+		#so instead of adding delay, it just chcks for <= 1 enemy
+		if(main_path.get_child_count()<=1):
+			print("All Enemies Defeated")
 
 		
