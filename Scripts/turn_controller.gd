2746927135932e498a194685fd73045b54d9b9c7
Zane <zagrismore@gmail.com>
2024-09-21 21:41:01 -0400

    Improved Turn Controller

--- Scripts/turn_controller.gd
+++ Scripts/turn_controller.gd
@@ -1,8 +1,10 @@
 extends Area2D
 
-@export var turnType:String ="left"
+enum direction {Left, Right}
+
+@export var turnDirection : direction
 
 func _on_body_entered(body:Node2D):
-    print(body.get_class)
-    if body.is_class("enemy") == true:
-        print("yay")
+	print(body.get_class)
+	if body.is_class("enemy") == true:
+		print("yay")
