16382e9fc54e2d61adb5d9cf59bf9f97aca6c054
Zane <zagrismore@gmail.com>
2024-09-21 22:25:21 -0400

    Placed enemy turners

--- Scripts/enemyClass.gd
+++ Scripts/enemyClass.gd
@@ -3,6 +3,7 @@ class_name Enemy
 
 const SPEED = 50.0
 
+@export var speedMultiplier :int =1
 
 func _physics_process(delta):
-	get_parent().progress += delta*SPEED
+	get_parent().progress += delta*SPEED*speedMultiplier
