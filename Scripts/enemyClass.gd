07eacaf81fb5c221d0fb1714735f5f1f45befc52
Zane <zagrismore@gmail.com>
2024-09-21 22:22:21 -0400

    Improved rendering
    Things render around the enemy properly

--- Scripts/enemyClass.gd
+++ Scripts/enemyClass.gd
@@ -1,9 +1,8 @@
 extends CharacterBody2D
-class_name enemy
+class_name Enemy
 
 const SPEED = 50.0
 
 
 func _physics_process(delta):
 	get_parent().progress += delta*SPEED
-
--- Scripts/turn_controller.gd
+++ Scripts/turn_controller.gd
@@ -1,10 +1,20 @@
 extends Area2D
 
-enum direction {Left, Right}
-
-@export var turnDirection : direction
+enum hDirection {Left, Right}
+enum vDirection {Down,Up}
+@export var turnH : hDirection
+@export var turnV : vDirection
+#Exports diffent directions for me to confiure each controler to 
 
 func _on_body_entered(body:Node2D):
-	print(body.get_class)
-	if body.is_class("enemy") == true:
-		print("yay")
+	if body.name=="Enemy":
+	#Makes sure it's an enemy
+		if turnH == hDirection.Left:
+			body.get_child(0).flip_h= false
+		elif turnH == hDirection.Right:
+			body.get_child(0).flip_h= true
+		if turnV == vDirection.Up:
+			body.get_child(0).play("Up")
+		elif turnV == vDirection.Down:
+			body.get_child(0).play("Down")
+	#Each line above flips the sprite and/or
