4816782c38065a2f1efe8bf6cf9bc3a8d1e6367f
Zane <zagrismore@gmail.com>
2024-09-21 22:43:35 -0400

    Added in damage functionality

--- Scripts/enemyClass.gd
+++ Scripts/enemyClass.gd
@@ -3,7 +3,20 @@ class_name Enemy
 
 const SPEED = 50.0
 
+@export var health : int = 1
 @export var speedMultiplier :int =1
 
+@export var hurtbox : CollisionShape2D
+
+func _ready() -> void:
+	pass
 func _physics_process(delta):
 	get_parent().progress += delta*SPEED*speedMultiplier
+	##moves enemy on path
+func takeDamage (amount:int):
+	health-=amount
+	print(health)
+	#lose health
+	if health<=0:
+		queue_free()
+		#deletes enemy if it's dead
