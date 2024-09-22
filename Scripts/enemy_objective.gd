8a76acb36028bd58530b9a06b3a6456b60d5d2f5
Zane <zagrismore@gmail.com>
2024-09-21 23:07:45 -0400

    Started wave detection

--- Scripts/enemy_objective.gd
+++ Scripts/enemy_objective.gd
@@ -3,5 +3,12 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	if(body.name == "Enemy"):
 		#life losing goes here
-		body.queue_free()
+		print(body.get_parent().get_child_count())
+		if(body.get_parent().get_child_count()==0):
+			print("All Enemies Defeated")
+		body.get_parent().queue_free()
+		#deletes the enemy from the scene
 		print("Enemy has made it!")
+		print(body.get_parent().get_children())
+
+		
